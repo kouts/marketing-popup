26afb3dcd7e3557cbcd0e6a8cbbd5f5bd5edd2cf
E6440\Kouts <koutsaftakis@gmail.com>
2020-08-16 21:22:55 +0300

    Fixed missing modal styles from build

--- postcss.config.js
+++ postcss.config.js
@@ -8,8 +8,8 @@ module.exports = {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
         return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
       },
-      whitelist: [],
-      whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/]
+      whitelist: ['vm'],
+      whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/, /vm-.*/]
     })
   ]
 };
