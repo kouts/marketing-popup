a56aadd8c4433f95e67fcf38c2e52d9d37675e46
E6440\Kouts <koutsaftakis@gmail.com>
2020-08-23 13:12:29 +0300

    Added notyf styles to purge css whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -9,8 +9,16 @@ module.exports = {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
         return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
       },
-      whitelist: ['vm'],
-      whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/, /vm-.*/]
+      whitelist: ['vm', 'notyf'],
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+        /vm-.*/,
+        /notyf-.*/,
+        /notyf__.*/
+      ]
     })
   ]
 };
